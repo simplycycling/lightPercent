597e743e6f323206e64d8e48ca66a4f1b1c6f631
Roger Sherman <rsherman@linux.com>
2017-09-09 12:15:43 +1000

    Added function to convert the current brightness value into a percentage of the max value.

--- main.go
+++ main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+
 )
+
+//maxBrightness := 937
+//current := func now() int {
+//	a, err := ioutil.ReadFile("/sys/class/backlight/intel_backlight/brightness")
+//	check(err)
+//	s := string(a)
+//	i, err := strconv.Atoi(s)
+//	return i
+//}
+
+func percentage(current int) float64 {
+	const max = 937
+	percent := (float64(current) * float64(100)) / float64(max)
+	return percent
+}
+
+func main() {
+	fmt.Println(percentage(504))
+}
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
-}
-
-func main() {
-	a, err := ioutil.ReadFile("/sys/class/backlight/intel_backlight/brightness")
-	check(err)
-	fmt.Println(string(a))
 }
